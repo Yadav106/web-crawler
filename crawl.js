e3344d82a4d836e5ca2672229aa8d8c72a61a86b
Yadav106 <sahil.jp.yadav@gmail.com>
2023-02-21 21:25:25 +0530

    getURLsFromHTML function and tests

--- crawl.js
+++ crawl.js
@@ -1,3 +1,40 @@
+const { JSDOM } = require('jsdom');
+
+function getURLsFromHTML(htmlBody, baseURL) {
+    const urls = [];
+    const dom = new JSDOM(htmlBody);
+    const linkElements = dom.window.document.querySelectorAll('a');
+    for (const linkElement of linkElements) {
+        if(linkElement.href.slice(0, 1) === '/') {
+            // relative
+            if(baseURL.slice(-1) === '/') {
+                try {
+                    const url = new URL(`${baseURL.slice(0, -1)}${linkElement.href}`);
+                    urls.push(url.href);
+                } catch(err) {
+                    console.log(`error in relative url : ${err}`);
+                }
+            } else {
+                try {
+                    const url = new URL(`${baseURL}${linkElement.href}`);
+                    urls.push(url.href);
+                } catch(err) {
+                    console.log(`error in relative url : ${err}`);
+                }
+            }
+        } else {
+            // absolute
+            try {
+                const url = new URL(linkElement.href);
+                urls.push(url.href)
+            } catch(err) {
+                console.log(`error in absolute url : ${err}`);
+            }
+        }
+    }
+    return urls;
+}
+
 function normalizeURL(urlString){
     const UrlObj = new URL(urlString);
     const hostPath = `${UrlObj.hostname}${UrlObj.pathname}`;
@@ -8,5 +45,6 @@ function normalizeURL(urlString){
 }
 
 module.exports = {
-    normalizeURL
+    normalizeURL,
+    getURLsFromHTML
 }
