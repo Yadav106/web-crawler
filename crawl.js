c6ea9cae4ac0312b8a114c50a5241b9e6492d3d5
Yadav106 <sahil.jp.yadav@gmail.com>
2023-02-21 20:14:28 +0530

    added tests
    normalizeURL and tests

---
+++ crawl.js
@@ -0,0 +1,12 @@
+function normalizeURL(urlString){
+    const UrlObj = new URL(urlString);
+    const hostPath = `${UrlObj.hostname}${UrlObj.pathname}`;
+    if (hostPath.length > 0 && hostPath.slice(-1) === '/') {
+        return hostPath.slice(0, -1);
+    }
+    return hostPath;
+}
+
+module.exports = {
+    normalizeURL
+}
